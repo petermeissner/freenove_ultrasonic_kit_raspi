a41ab2dd53fc91dc4ad84db5f2db4eb7168f9c2f
petermeissner <retep.meissner@gmail.com>
2019-05-05 21:10:56 +0200

    Blink.R with pin mode

--- Code/R_Code/01.1.1_Blink/Blink.R
+++ Code/R_Code/01.1.1_Blink/Blink.R
@@ -5,6 +5,7 @@ library(RpiR)
 
 # initialization overhead
 RpiR::init()
+RpiR::pin_mode(0, "out")
 
 
 # blink once function
@@ -12,11 +13,11 @@ blink_once <- function (on_s = 1, off_s = 1) {
 	# make sure LED is turned off on exit
 	on.exit( write_digital(0, 0) )
 	
-	# turn LED on and wait 1s
+	# turn LED on and wait 
 	write_digital(0, 1)
 	Sys.sleep(on_s)
 	
-	# turn LED off and wait 1s
+	# turn LED off and wait
 	write_digital(0, 0)
 	Sys.sleep(off_s)
 }
