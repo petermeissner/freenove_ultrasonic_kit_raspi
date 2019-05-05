a78414961207ff7a3e8d5610452281d56a151081
petermeissner <retep.meissner@gmail.com>
2019-05-05 18:15:20 +0200

    Blink.c typos

--- Code/C_Code/01.1.1_Blink/Blink.c
+++ Code/C_Code/01.1.1_Blink/Blink.c
@@ -11,20 +11,26 @@
 
 int main(void)
 {
-	if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
+	if(wiringPiSetup() == -1){ // When initialize wiring failed, print message to screen
 		printf("setup wiringPi failed !");
 		return 1; 
 	}
-	//when initialize wiring successfully,print message to screen	
-	printf("wiringPi initialize successfully, GPIO %d(wiringPi pin)\n",ledPin); 	
 	
-	pinMode(ledPin, OUTPUT);//Set the pin mode
+	// When initialize wiring successfully, print message to screen	
+	printf("wiringPi initialize successfully, GPIO %d(wiringPi pin)\n", ledPin); 	
+	
+	// Set the pin mode
+	pinMode(ledPin, OUTPUT); 
 
 	while(1){
-			digitalWrite(ledPin, HIGH);  //led on
+		
+			//led on
+			digitalWrite(ledPin, HIGH);  
 			printf("led on...\n");
 			delay(1000);
-			digitalWrite(ledPin, LOW);  //led off
+			
+			//led off
+			digitalWrite(ledPin, LOW);  
 			printf("...led off\n");
 			delay(1000);
 	}
