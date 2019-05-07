ec86857cc1bc9b49dd391351fe25659ca3fb18c2
petermeissner <retep.meissner@gmail.com>
2019-05-07 08:21:48 +0200

    blink on different pin

---
+++ Code/C_Code/01.1.1_Blink/Blink_pin_2.c
@@ -0,0 +1,40 @@
+/**********************************************************************
+* Filename    : Blink.c
+* Description : Make an led blinking.
+* auther      : www.freenove.com
+* modification: 2016/06/07
+**********************************************************************/
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define  ledPin    2
+
+int main(void)
+{
+	if(wiringPiSetup() == -1){ // When initialize wiring failed, print message to screen
+		printf("setup wiringPi failed !");
+		return 1; 
+	}
+	
+	// When initialize wiring successfully, print message to screen	
+	printf("wiringPi initialize successfully, GPIO %d(wiringPi pin)\n", ledPin); 	
+	
+	// Set the pin mode
+	pinMode(ledPin, OUTPUT); 
+
+	while(1){
+		
+			//led on
+			digitalWrite(ledPin, HIGH);  
+			printf("led on...\n");
+			delay(1000);
+			
+			//led off
+			digitalWrite(ledPin, LOW);  
+			printf("...led off\n");
+			delay(1000);
+	}
+
+	return 0;
+}
+
